9b57f4009f012081330e58614499370c1258e428
sffee <sffeee@naver.com>
2023-08-31 19:05:58 +0900

    [슴도고치] UnrealMusume 프로젝트 추가 (ºДº)

---
+++ Source/UnrealMusume/UnrealMusumeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealMusumeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALMUSUME_API AUnrealMusumeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
