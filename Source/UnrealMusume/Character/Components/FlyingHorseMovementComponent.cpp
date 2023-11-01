e394e92ed15bce98440ea3dbd79d7484e637f7e6
leehwoo0119 <70616571+leehwoo0119@users.noreply.github.com>
2023-11-01 16:12:03 +0900

    [칸따삐야] 기능 온오프 추가 1차

--- Source/UnrealMusume/Character/Components/FlyingHorseMovementComponent.cpp
+++ Source/UnrealMusume/Character/Components/FlyingHorseMovementComponent.cpp
@@ -14,24 +14,42 @@ void UFlyingHorseMovementComponent::BeginPlay()
 {
 	Super::BeginPlay();
 
-	OwingCharacter->GetCharacterMovement()->SetMovementMode(MOVE_Flying);
+	ExecuteFunction(true);
 }
 
 void UFlyingHorseMovementComponent::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
 {
 	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
 
-	FVector start = OwingCharacter->GetActorLocation();
-	FVector end = start + FVector(0.f, 0.f, -1000.f);
-
-	FCollisionQueryParams params;
-	params.AddIgnoredActor(OwingCharacter);
-
-	FHitResult hitResult;
-	bool isHit = GetWorld()->LineTraceSingleByChannel(hitResult, start, end, ECC_WorldStatic, params);
-	if (isHit)
+	if (IsFlyingOn)
 	{
-		FVector target = UKismetMathLibrary::VInterpTo(OwingCharacter->GetActorLocation(), hitResult.ImpactPoint + FVector(0.f, 0.f, FlyingTargetZ), DeltaTime, ZInterpSpeed);
-		OwingCharacter->SetActorLocation(target);
+		FVector start = OwingCharacter->GetActorLocation();
+		FVector end = start + FVector(0.f, 0.f, -1000.f);
+
+		FCollisionQueryParams params;
+		params.AddIgnoredActor(OwingCharacter);
+
+		FHitResult hitResult;
+		bool isHit = GetWorld()->LineTraceSingleByChannel(hitResult, start, end, ECC_WorldStatic, params);
+		if (isHit)
+		{
+			FVector target = UKismetMathLibrary::VInterpTo(OwingCharacter->GetActorLocation(), hitResult.ImpactPoint + FVector(0.f, 0.f, FlyingTargetZ), DeltaTime, ZInterpSpeed);
+			OwingCharacter->SetActorLocation(target);
+		}
 	}
+}
+
+void UFlyingHorseMovementComponent::ExecuteFunction_Implementation(bool _IsFunctionOn)
+{
+	if (_IsFunctionOn)
+	{
+		OwingCharacter->GetCharacterMovement()->SetMovementMode(MOVE_Flying);
+		IsFlyingOn = true;
+	}
+	else
+	{
+		OwingCharacter->GetCharacterMovement()->SetMovementMode(MOVE_Walking);
+		IsFlyingOn = false;
+	}
+	
 }
